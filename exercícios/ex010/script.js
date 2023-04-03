93b36f35125348a010bbd02ee2b5dcf9628d2dd4
Felipe Augusto <126160371+felipeaugustoap@users.noreply.github.com>
2023-04-04 01:24:53 +0200

    ex

---
+++ exercícios/ex010/script.js
@@ -0,0 +1,18 @@
+
+var res = window.document.getElementById('resultado')
+
+function açao1(){
+    res.innerHTML += '<p>Você clicou no primeiro botão.</p>'
+}
+
+function açao2(){
+    res.innerHTML += '<p>Você clicou no segundo botão.</p>'
+}
+
+function açao3(){
+    res.innerHTML += '<p>Você clicou no terceiro botão.</p>'
+}
+
+function açao4(){
+    res.innerHTML += '<p>Você clicou no quarto botão.</p>'
+}
